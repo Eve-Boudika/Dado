39e730945221ecf51f84c6bf94f602063a35f15b
Evelyn <evelabohemia@gmail.com>
2022-07-25 21:46:09 -0300

    Subida primigenia de archivos

---
+++ LabDadoProbabilidad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LabDadoProbabilidad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            Console.WriteLine("Ingrese la cantidad de veces que lanzará el dado");
+
+            int CantidadDeTiradas = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < CantidadDeTiradas; i++)
+            {
+                Console.Write("Sale Nº " + random.Next(1, 7) + " ");
+
+                Console.WriteLine("Tirada N° {0}", i + 1);
+            }
+
+
+
+        }
+    }
+}
